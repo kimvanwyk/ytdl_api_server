6f5a0c4d84443369c340454f9a615433bb996096
Kim van Wyk <kimv@sahomeloans.com>
2021-02-06 21:28:41 +0200

    Add URL adding capacity to body

--- ytdl_api_server/main.py
+++ ytdl_api_server/main.py
@@ -1,8 +1,22 @@
+from db import DB
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+DBH = DB()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+class UrlModel(BaseModel):
+    url: str
+
+
+@app.get("/urls/pending")
+def pending():
+    return DBH.get_pending_urls()
+
+
+@app.post("/urls/add")
+def add(url: UrlModel):
+    DBH.add_url(url.url)
