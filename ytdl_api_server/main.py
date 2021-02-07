083c60c71e68e9e53733ea8c23b0182559a0caf4
Kim van Wyk <kimv@sahomeloans.com>
2021-02-07 17:13:16 +0200

    Add documentation

--- ytdl_api_server/main.py
+++ ytdl_api_server/main.py
@@ -14,9 +14,15 @@ class UrlModel(BaseModel):
 
 @app.get("/urls/pending")
 def pending():
+    '''Return IDs and URLs of yet-to-be downloaded URLs
+    '''
+
     return DBH.get_pending_urls()
 
 
 @app.post("/urls/add")
 def add(url: UrlModel):
+    ''' Add a new URL for downloading
+    '''
+
     DBH.add_url(url.url)
